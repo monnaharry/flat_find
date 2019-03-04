3b493ca8b1a569913d2600c2be1f46bd8cbe8ffb
Rebecca Green <rebeccalynngreen@gmail.com>
2019-03-04 18:17:27 +0100

    add validations to User, Reservation, and Flat models. Constructed routes

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,9 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :reservations
+  has_many :users, through: :reservations
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :flats, only: [:index, :show]
+
+  resources :users do
+    resources :reservations, only: [:edit, :update, :destroy]
+    resources :flats, only: [:create, :new, :edit, :update, :destroy] do
+      resources :reservations, only: [:create, :new]
+    end
+  end
 end
