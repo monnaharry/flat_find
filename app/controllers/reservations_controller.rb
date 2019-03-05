59efa7403caddc968dd0f80a8467c75c044f5d79
Pontus Nordbrandt <pontus.nordbrandt@gmail.com>
2019-03-05 18:01:21 +0100

    added method for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,16 +3,29 @@ class ReservationsController < ApplicationController
   end
 
   def new
-    raise
+    @reservation = Reservation.new
   end
 
   def destroy
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    @flat = Flat.find(params[:flat_id])
+    @reservation.flat = @flat
+    if @reservation.save
+      redirect_to flats_path(@flat)
+    else
+      render :create
+    end
   end
 
   def update
   end
 
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
