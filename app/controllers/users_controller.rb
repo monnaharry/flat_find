614096942877933e957c1323ea48c555e4cac0d8
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-06 16:35:13 +0100

    add uploadProfilePic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,17 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    @user.save
+    redirect_to user_path(@user)
   end
 
   def delete
+  end
+
+  def user_params
+    params.require(:user).permit!
   end
 
 end
