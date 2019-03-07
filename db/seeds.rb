136bc7067a3860c50b05777707bf7ebdbd3fc675
Jordi Gramunt <jordi.gramunt@gmail.com>
2019-03-07 12:26:20 +0100

    modified seeds to have admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,22 +26,37 @@ puts 'Generating fake data...'
   user.password_confirmation = 'password'
   user.birth_date = Faker::Date.birthday(18, 65)
   user.phone_number = Faker::PhoneNumber.cell_phone
-  user.remote_profile_pic_url = "https://source.unsplash.com/collection/3107814/#{(1..150).to_a.sample}"
+  # user.remote_profile_pic_url = "https://source.unsplash.com/collection/3107814/#{(1..150).to_a.sample}"
   user.save
 end
 
-5.times do |flat|
-  flat = Flat.new
-  flat.name = Faker::Construction.material + " " + %w(hut bungalow cottage ranch tower housebarn).sample
-  flat.address = Faker::Address.full_address
-  flat.description = Faker::Hipster.paragraphs([1, 2, 3].sample, true).join("\n")
-  flat.price = (10..300).to_a.sample
-  flat.user_id = User.all.sample.id
-  flat.save
-  (3..5).to_a.sample.times do |image|
-    image = Image.new
-    image.remote_url_url = "https://source.unsplash.com/collection/1134892/#{(1..150).to_a.sample}"
-    image.flat_id = flat.id
-    image.save
-  end
-end
+# 5.times do |flat|
+#   flat = Flat.new
+#   flat.name = Faker::Construction.material + " " + %w(hut bungalow cottage ranch tower housebarn).sample
+#   flat.address = Faker::Address.full_address
+#   flat.description = Faker::Hipster.paragraphs([1, 2, 3].sample, true).join("\n")
+#   flat.price = (10..300).to_a.sample
+#   flat.user_id = User.all.sample.id
+#   flat.save
+#   (3..5).to_a.sample.times do |image|
+#     image = Image.new
+#     image.remote_url_url = "https://source.unsplash.com/collection/1134892/#{(1..150).to_a.sample}"
+#     image.flat_id = flat.id
+#     image.save
+#   end
+# end
+
+puts 'Adding admin user'
+
+admin = User.new()
+admin.name = "Admin"
+admin.email = "admin@admin.com"
+admin.gender = "prefer not to say"
+admin.password = 'admin123'
+admin.password_confirmation = 'admin123'
+admin.birth_date = Faker::Date.birthday(18, 65)
+admin.phone_number = Faker::PhoneNumber.cell_phone
+admin.remote_profile_pic_url = "https://i.imgur.com/1tpvNmh.jpg"
+admin.admin = true
+admin.save!
+
