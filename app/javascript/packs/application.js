b27a493ac6f307bf14f645a486530be1e57cfe04
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-07 18:25:53 +0100

    add search bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,4 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 initMapbox();
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
