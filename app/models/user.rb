cb6a33dc18766523e8b9383b8eb84949b2f00357
Jordi Gramunt <jordi.gramunt@gmail.com>
2019-03-08 14:43:49 +0100

    fixed default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  mount_uploader :profile_pic, PhotoUploader
   has_many :flats, dependent: :destroy
   has_many :reservations, dependent: :destroy
   validates :name, presence: true
   after_create :adding_default_avatar
+  mount_uploader :profile_pic, PhotoUploader
 
   def adding_default_avatar
-    if self.profile_pic.nil?
       self.remote_profile_pic_url = "https://www.drupal.org/files/issues/default-avatar.png"
       self.save
-    end
   end
 end
