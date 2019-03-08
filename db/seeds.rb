cd6868eb4ce6a1aae20b7049b715b9c540bc0295
Julia Shelley <j.c.shelley@btinternet.com>
2019-03-08 14:57:16 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,9 +47,12 @@ end
     description: "Enjoy Lisbon, while living in a new, apartment. It is centrally-heated apartment in a central historic part of town!",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Shared room",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
     Flat.create!({
@@ -58,9 +61,12 @@ end
     description: "Luna Parque is a typical Lisbon apartment of the 40s, full of atmosphere that will remind you to a magical place. The wide dining areas overlook the private garden, a perfect place to chill out in the city center.",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Private room",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
     Flat.create!({
@@ -69,9 +75,12 @@ end
     description: "If you want to experience the locals way of living, this is the place to be!",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Entire place",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
     Flat.create!({
@@ -80,9 +89,12 @@ end
       description: "Charming city view apartment on Lisbon's beautiful Castle hill",
       price: (10..300).to_a.sample,
       user_id: User.all.sample.id,
-      longitude: rand(2.002..2.222).round(5),
-      latitude: rand(41.012..41.722).round(5),
+      # longitude: rand(2.002..2.222).round(5),
+      # latitude: rand(41.012..41.722).round(5),
       room_type: "Shared room",
+      bathroom: rand(1..5),
+      bed: rand(1..5),
+      guest: rand(1..5),
   })
 
     Flat.create!({
@@ -91,9 +103,12 @@ end
       description: "Stylish and cozy apartment on Lisbon's hillside with sea views. Situated in the historical “Graça” neighbourhood, on the highest of the seven hills of Lisbon, this sunny apartment recently renovated with modern appliances.",
       price: (10..300).to_a.sample,
       user_id: User.all.sample.id,
-      longitude: rand(2.002..2.222).round(5),
-      latitude: rand(41.012..41.722).round(5),
+      # longitude: rand(2.002..2.222).round(5),
+      # latitude: rand(41.012..41.722).round(5),
       room_type: "Entire place",
+      bathroom: rand(1..5),
+      bed: rand(1..5),
+      guest: rand(1..5),
   })
 
     Flat.create!({
@@ -102,9 +117,12 @@ end
     description: "Authentic Cuban newly renovated apartment. Close to the main attractions and old Havana. Restaurants, bars, supermarkets and tourist attractions all in walking distance.",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Entire place",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5)
   })
 
   Flat.create!({
@@ -113,9 +131,12 @@ end
     description: "I can't describe how you will feel staying here in this stylish bedroom located on the roof top in Old Havana with awe inspiring views to the city and the ocean",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Private room",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
   Flat.create!({
@@ -124,9 +145,12 @@ end
     description: "Share drinks and conversation under the stars outside this lovely Old Havana home",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Private room",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
 Flat.create!({
@@ -135,9 +159,12 @@ Flat.create!({
     description: "Wake up and step out onto the sun-drenched terraces for coffee, tea, or natural juice. Near to all sightseeing, restaurants, museums, galleries art, bars, etc.",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Entire place",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
 Flat.create!({
@@ -146,9 +173,12 @@ Flat.create!({
     description: "We are on the road to Soroa in the Frias neighborhood where you can enjoy beautiful views, take advantage of and share with us the opportunities of life in the countryside. ",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Private room",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
 Flat.create!({
@@ -157,9 +187,12 @@ Flat.create!({
     description: "Our house is located 2 miles from the Soroa that has the largest collection of orchids in Cuba, in this house there is an incredible tranquility in addition to being surrounded by the best garden of ornamental plants and around us there are several endemic birds",
     price: (10..300).to_a.sample,
     user_id: User.all.sample.id,
-    longitude: rand(2.002..2.222).round(5),
-    latitude: rand(41.012..41.722).round(5),
+    # longitude: rand(2.002..2.222).round(5),
+    # latitude: rand(41.012..41.722).round(5),
     room_type: "Entire place",
+    bathroom: rand(1..5),
+    bed: rand(1..5),
+    guest: rand(1..5),
   })
 
 
